dd953959afb8b6371a12da45857e7749523ed136
RSoph <kate.brender@gmail.com>
2015-06-30 00:27:05 -0400

    testing

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -1,9 +1,9 @@
 class RsvpController < ApplicationController
 
 	def index
-		client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-		message = client.messages.create from: '15675234372', to: '17187535492', body: 'hello world'
-		render plain: message.status
+		# client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+		# message = client.messages.create from: '15675234372', to: '17187535492', body: 'hello world'
+		# render plain: message.status
 
   		# twiml = Twilio::TwiML::Response.new do |r|
 		  # r.Message "Hello world!"
