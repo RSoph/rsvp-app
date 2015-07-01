3de7691da4219906d29304fedce7209a9ae95a12
RSoph <kate.brender@gmail.com>
2015-06-30 20:19:40 -0400

    customized response

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -11,10 +11,21 @@ class RsvpController < ApplicationController
 	  		message = client.messages.create(
 		    from: "+15675234372",
 		    to: "#{from_number}",
-		    body: "Hey there! I got a text from you, it said #{message_body}"
+		    body: "Great to hear from you! Please let us know: your name, if you will be attending the wedding, if you will be attending the Friday Night Reception (all are welcome!), and how many people you'll be bringing including yourself."
+	  		)
+	  	else
+	  		message_body = params["Body"]
+			message_body ||= "just testing"
+			from_number = params["From"]
+			from_number ||= '7187535492'
+			client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+
+	  		message = client.messages.create(
+		    from: "+15675234372",
+		    to: "#{from_number}",
+		    body: "Thanks for the response! If your RSVP changes, you can always text again."
 	  		)
 	  	end
-
 	end
 
 	def notify
