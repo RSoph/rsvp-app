d14ede87546dd9c2c00590e356b11b2a35c8ca7c
RSoph <kate.brender@gmail.com>
2015-06-30 20:04:29 -0400

    testing

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -11,18 +11,18 @@ class RsvpController < ApplicationController
   		# twiml.text
   		# binding.pry
 
-
 		message_body = params["Body"]
 		message_body ||= "just testing"
 		from_number = params["From"]
 		from_number ||= '7187535492'
 		client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
 
-  		client.account.sms.messages.create(
+  		message = client.messages.create(
 	    from: "+15675234372",
-	    to: "+1#{from_number}",
+	    to: "#{from_number}",
 	    body: "Hey there! I got a text from you, it said #{message_body}"
   		)
+  		
 	end
 
 	def notify
@@ -33,3 +33,7 @@ class RsvpController < ApplicationController
       params.permit(:message_body, :from_number)
     end
 end
+
+
+# message = client.messages.create( from: "+15675234372", to: "+17187535492", body: "console texting!")
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
+  root 'rsvp#index'
+
 
   get '/sms-quickstart' => 'rsvp#index'
   post '/sms-quickstart' => 'rsvp#index'
 
-  root 'rsvp#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
