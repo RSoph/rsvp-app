ec07b5b650576ea31bb9b634addb5bae135b74c3
RSoph <kate.brender@gmail.com>
2015-06-30 20:47:56 -0400

    split the long message

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -1,25 +1,28 @@
 class RsvpController < ApplicationController
 
 	def index
-		if params["Body"].downcase.strip == 'rsvp'
-			message_body = params["Body"]
-			message_body ||= "just testing"
-			from_number = params["From"]
-			from_number ||= '7187535492'
-			client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
 
+		enable :sessions
+		session["counter"] ||= 0
+
+		message_body = params["Body"]
+		message_body ||= "just testing"
+		from_number = params["From"]
+		from_number ||= '7187535492'
+		client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+
+		if params["Body"].downcase.strip == 'rsvp'
 	  		message = client.messages.create(
 		    from: "+15675234372",
 		    to: "#{from_number}",
-		    body: "Great to hear from you! Please let us know: your name, if you will be attending the wedding, if you will be attending the Friday Night Reception (all are welcome!), and how many people you'll be bringing including yourself."
+		    body: "Great to hear from you! Please let us know the following: 1) Your name 2) If you will be attending the wedding..."
 	  		)
-	  	else
-	  		message_body = params["Body"]
-			message_body ||= "just testing"
-			from_number = params["From"]
-			from_number ||= '7187535492'
-			client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-
+	  		message = client.messages.create(
+		    from: "+15675234372",
+		    to: "#{from_number}",
+		    body: "...3) If you will be attending the Friday Night Reception - all are welcome! 4) How many people you'll be bringing including yourself."
+	  		)
+	  	elsif session["counter"] > 0
 	  		message = client.messages.create(
 		    from: "+15675234372",
 		    to: "#{from_number}",
