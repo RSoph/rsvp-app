27372eb3bc4b0d7ed2f2a45696b5c763b832f3f4
RSoph <kate.brender@gmail.com>
2015-06-30 20:15:47 -0400

    only if you text rsvp

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -1,18 +1,19 @@
 class RsvpController < ApplicationController
 
 	def index
+		if params["Body"].downcase.strip == 'rsvp'
+			message_body = params["Body"]
+			message_body ||= "just testing"
+			from_number = params["From"]
+			from_number ||= '7187535492'
+			client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
 
-		message_body = params["Body"]
-		message_body ||= "just testing"
-		from_number = params["From"]
-		from_number ||= '7187535492'
-		client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-
-  		message = client.messages.create(
-	    from: "+15675234372",
-	    to: "#{from_number}",
-	    body: "Hey there! I got a text from you, it said #{message_body}"
-  		)
+	  		message = client.messages.create(
+		    from: "+15675234372",
+		    to: "#{from_number}",
+		    body: "Hey there! I got a text from you, it said #{message_body}"
+	  		)
+	  	end
 
 	end
 
