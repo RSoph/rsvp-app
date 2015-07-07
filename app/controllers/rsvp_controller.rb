306d75b1614e96b366118c548ef3b70e8a162f98
RSoph <kate.brender@gmail.com>
2015-07-06 23:29:47 -0400

    added a conversation route

--- app/controllers/rsvp_controller.rb
+++ app/controllers/rsvp_controller.rb
@@ -1,20 +1,18 @@
 class RsvpController < ApplicationController
 
-	MY_NUMBER = "+17187535492"
-	TO_EMAIL = "theladiesb@gmail.com"
-	# SMTP_HOST = "<SMTP hostname here>"
-	# SMTP_USER = "<your username here>"
-	# SMTP_PASSWORD = "<your password here>"
+
+
 
 	def index
 
 		message_body = params["Body"]
 		message_body ||= "just testing"
 		from_number = params["From"]
-		from_number ||= '7187535492'
+
+
 		client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
 
-		if params["Body"].downcase.strip == 'rsvp'
+		if message_body.downcase.strip == 'rsvp'
 	  		message = client.messages.create(
 		    from: "+15675234372",
 		    to: "#{from_number}",
@@ -35,47 +33,96 @@ class RsvpController < ApplicationController
 	  		message = client.messages.create(
 		    from: "+15675234372",
 		    to: "7187535492",
-		    body: "incoming rsvp: #{params["Body"]}"
+		    body: "incoming rsvp: #{message_body}"
 	  		)
 	  	end
+		render nothing: true
 	end
 
-	def notify
+	def count
+		@texts = ["Great to hear from you! What's your name?", 
+				"Hey #{firstname}, will you be able to come to Kate and Danielle's wedding? Please give me a 'yes' or a 'no'", 
+				"Great! Will you also join us for the Friday Night Reception? Please give me a 'yes' or a 'no'", 
+				"That's too bad, you will be missed! If anything changes, feel free to text 'rsvp' again to start over!", 
+				"Fantastic! How many other people are coming with you? Just a number will do.", 
+				"That's too bad, you'll be missed. How many other people are coming with you to the wedding?", 
+				"Great! Looking forward to seeing you. If anything changes, feel free to text 'rsvp' again to start over!",
+				"I didn't get that, could you try again with just a 'yes' or a 'no'?"]
+		message_body = params["Body"]
+		message_body ||= "just testing"
+		from_number = params["From"]
+		from_number ||= '7187535492'
+		from_hash ||= {}
+		from_hash[from_number] ||= 0
+		client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
+
+		if message_body.downcase.strip == 'rsvp' # beginning
+			from_hash[from_number] = 1
+	  		message = client.messages.create(
+		    from: "+15675234372",
+		    to: "#{from_number}",
+		    body: @texts[0]
+	  		)
+		elsif from_hash[from_number] == 1 # what's your name?
+			from_hash[from_number] += 1
+			firstname = body.split(" ")[0]
+	  		message = client.messages.create(
+		    from: "+15675234372",
+		    to: "#{from_number}",
+		    body: @texts[1]
+	  		)
+		elsif from_hash[from_number] == 2 # are you coming to the wedding?
+			if message_body.downcase.strip == 'yes' # move on to the next question
+				from_hash[from_number] += 1
+				message = client.messages.create(
+			    from: "+15675234372",
+			    to: "#{from_number}",
+			    body: @texts[2]
+		  		)
+			elsif message_body.downcase.strip = 'no' # reset sessions to 0 so they can start over if necessary
+				from_hash[from_number] = 0
+				message = client.messages.create(
+			    from: "+15675234372",
+			    to: "#{from_number}",
+			    body: @texts[3]
+		  		)				
+			else	# didn't understand, do not change sessions
+				message = client.messages.create(
+			    from: "+15675234372",
+			    to: "#{from_number}",
+			    body: @texts[7]
+		  		)	
+		  	end
+		elsif from_hash[from_number] = 3 # are you coming to friday?
+			if message_body.downcase.strip == 'yes'
+				from_hash[from_number] += 1
+				message = client.messages.create(
+			    from: "+15675234372",
+			    to: "#{from_number}",
+			    body: @texts[4]
+		  		)	
+			elsif message_body.downcase.strip = 'no'
+				from_hash[from_number] += 1
+				message = client.messages.create(
+			    from: "+15675234372",
+			    to: "#{from_number}",
+			    body: @texts[5]
+		  		)	
+			else
+				message = client.messages.create(
+			    from: "+15675234372",
+			    to: "#{from_number}",
+			    body: @texts[7]
+		  		)
+			end
+		elsif from_hash[from_number] = 4 # how many people?
+				message = client.messages.create(
+			    from: "+15675234372",
+			    to: "#{from_number}",
+			    body: @texts[6]
+		  		)	
+		end
 	end
-
-	# def answer
-	#     Twilio::TwiML::Response.new do |r|
-	#         r.Say "Thanks for calling, please leave a message"
-	# 		r.Record :action => "/save-recording", :method => "get"
-	#         end
-	#     end.text
-	# end
-
-	# def save
-	#     send_message("New Message on the hotline",
-	#                  "Message length: #{params['RecordingDuration']}",
-	#                  params['RecordingUrl'] + '.mp3')
-	 
-	#     Twilio::TwiML::Response.new do |r|
-	#         r.Say "Thank you for calling. Bye!"
-	#         r.Hangup
-	#     end.text		
-	# end
-
-	# def send_message(subject, message, file_url)
- #    Pony.mail({
- #        :to => TO_EMAIL
- #        :body => message,
- #        :via => :smtp,
- #        :via_options => {
- #            # :address => SMTP_HOST,
- #            # :user_name => SMTP_USER,
- #            # :password => SMTP_PASSWORD,
- #            # :port => SMTP_PORT,
- #            :authentication => :plain },
- #        :attachments => {"message.mp3" => open(file_url) {|f| f.read }}
- #    })
-	# end
 
 	def rsvp_params
       params.permit(:message_body, :from_number)
